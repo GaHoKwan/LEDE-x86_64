a347f4bd2b8a29fc7a6fdc90516d173650f770c3
關嘉豪[GavinK♂luo2888] <kwangaho@gmail.com>
2021-04-30 23:05:46 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,4 +5,4 @@
 # Author: kenzo
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/10.0.30.2/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
